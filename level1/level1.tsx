b333f180ff90acad68ae84fe99304bf890178d0e
Atla004 <andrese.g.v.13579@gmail.com>
2024-03-19 14:49:55 -0400

    no

--- level1/level1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="level1" tilewidth="50" tileheight="32" tilecount="255" columns="17">
- <image source="level1.png" width="850" height="480"/>
-</tileset>
